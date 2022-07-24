3426d5e978b88eac0f33494215b3fa645e60f4d1
Hudson Retonde <hudsonsretonde@gmail.com>
2022-07-23 22:17:48 -0300

    Img capturada.

--- documentos/arquivos/data.js
+++ documentos/arquivos/data.js
@@ -2,5 +2,5 @@ window.audios = [{
     title: "Dragon Ball Z",
     cancao: "Cha-La Head-Cha-La",
     cover: "DBZ.jpg",
-    audio: "DragonBallZAbertura.mp3"
+    file: "DragonBallZAbertura.mp3"
 }];
--- documentos/arquivos/index.js
+++ documentos/arquivos/index.js
@@ -1,16 +1,16 @@
-const cover = document.querySelector(".card-image");
+const cover = document.querySelector(".card-image img");
 const title = document.querySelector(".card-content h6");
 const artist = document.querySelector(".card-content p");
-const audio = document.querySelector("audio");
+const audio = document.querySelector(".cart-content audio");
 
 const data = {
     title: "Dragon Ball Z",
     cancao: "Cha-La Head-Cha-La",
-    cover: "../../documentos/imgs/DBZ.jpg",
-    audio: "../../documentos/audio/DragonBallZAbertura.mp3"
+    cover: "../imgs/DBZ.jpg",
+    file: "../audio/DragonBallZAbertura.mp3"
 }
 
 cover.style.background = `url('${data.cover}')`;
 title.innerText = data.title;
 artist.innerText = data.cancao;
-audio.src = data.audio;  
+audio.src = data.file;  
